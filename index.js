fbc059947a4c29c8d093d9d7f62ae44488b0fcb6
Michael Lewis <mike@lew42.com>
2016-12-13 21:32:08 -0600

    yea

--- index.js
+++ index.js
@@ -1,92 +1,60 @@
 #!/usr/bin/env node
-// console.log("asdflkjadsfl");
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process.argv);
-// console.log(process.argv.slice(2));
-// console.log(process.cwd());
-var path = require("path");
+// #!/usr/bin/env node --inspect --debug-brk
 
+var path = require("path");
 var globule = require('globule');
-var cwd = process.cwd();
-console.log(cwd);
 
 var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
 
+var cwd = process.cwd();
+console.log('cwd:', cwd);
+
 var entry = {};
 
-// ugh
-// remove the cwd from filename?
+console.log("\nFiles:\n");
 
-var entryFiles = globule.find(cwd + "/**/*entry.js").forEach(function(filePath){
-	console.log(filePath, path.extname(filePath));
-	var entryName = filePath
-		.replace(path.extname(filePath), "")
-		.replace(".entry", "");
+// find anything in cwd (and subfolders) that ends in "entry.js"
+// we'll create bundles for these in the same folder, and swap "entry.js" for "bundle.js"
+var entryFiles = globule.find(cwd+"/**/*entry.js", "!" + cwd + "/node_modules/**").forEach(function(filePath){
+	filePath = path.resolve(filePath); // normalizes the slashes and stuff
+	console.log('filePath:', filePath);
 
-	console.log(entryName);
+	var relPath = path.relative(cwd, filePath);
+	console.log('relPath:', relPath);
 
-	entry[entryName] = ["webpack-dev-server/client?http://localhost:8080", filePath];
+	var entryName = relPath
+						.replace(path.extname(relPath), "")
+						.replace("entry", "");
+
+	entryName = entryName || "./"; // in case the file was named "entry.js", this fixed the problem
+
+	console.log('entryName:', entryName);
+
+	entry[entryName] = ["webpack-dev-server/client?http://localhost:8080/", filePath];
+	console.log("\n");
 });
+
+/*
+The entryName contains the relative path and part of the name (up until the "entry.js").
+This makes it easy to put the file back in its original spot, without having to create multiple configurations.
+*/
 
 var config = {
   devtool: 'inline-source-map',
   entry: entry,
   output: {
-  	path: "/",
-    filename: "[name].bundle.js"
+  	path: cwd,
+    filename: "[name]bundle.js"
   }
 };
 
-var compiler = webpack(config);
+var compiler = webpack(config, function(err, stats){
+	// debugger;
+});
+
 var server = new WebpackDevServer(compiler, {
-	contentBase: "/",
-	filename: "[name].bundle.js",
-	quiet: false,
-	publicPath: "/",
 	inline: true
 });
+
 server.listen(8080, "localhost");
-
-
-// var path = require("path");
-// var ExtractTextPlugin = require("extract-text-webpack-plugin");
-// // var MyPlugin = require("./MyPlugin");
-
-// var extractLESS = new ExtractTextPlugin("styles.css");
-
-// var entry = {
-//   // "./public/main": './src/entry.js'
-// };
-
-// var files = globule.find("./**/*.test.js").forEach(function(filePath){
-//   var entryName = filePath.replace(path.extname(filePath), "");
-//   entryName = entryName.replace(".entry", "").replace("./src", "");
-//   console.log(entryName);
-//   entry[entryName] = filePath;
-// });
-
-// module.exports = {
-//   devtool: 'inline-source-map',
-//   entry: "./entry.js",
-//   output: {
-//     filename: "[name]bundle.js"
-//   }
-//   // module: {
-//   //   loaders: [
-//   //     // { test: /\.css$/, exclude: /\.useable\.css$/, loader: "style!css" },
-//   //     // { test: /\.useable\.css$/, loader: "style/useable!css" },
-//   //     { test: /\.less$/, loader: extractLESS.extract("css-loader?sourceMap!less-loader?sourceMap") },
-//   //     { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=20000&mimetype=application/font-woff&name=/[hash].[ext]" },
-//   //     { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
-//   //   ]
-//   // }
-//   // ,plugins: [
-//   //   extractLESS
-//   // ]
-//   // ,
-//   // devServer: {
-//     // historyApiFallback: true
-//   // }
-// };
